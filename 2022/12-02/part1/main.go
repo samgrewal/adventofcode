a751efde55d7e7d324bbd5ef2ee279ceb4107c23
Sam Grewal <sam.grewal@wartsila.com>
2022-12-04 21:04:06 -0500

    12-02 part1

---
+++ 2022/12-02/part1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input.Close()
+	scores := [][]int{
+		{4, 8, 3},
+		{1, 5, 9},
+		{7, 2, 6},
+	}
+	total := 0
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		play := strings.Split(scanner.Text(), " ")
+		op := []byte(play[0])[0]
+		me := []byte(play[1])[0]
+		total += scores[op-65][me-88]
+	}
+	for scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+
+	println(strconv.Itoa(total))
+}
